4d09f19a47559f9c9433ed7b9836601b049b3d34
sardarabdulwakeel <107476362+sardarabdulwakeel@users.noreply.github.com>
2024-09-07 16:08:30 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-hello world
+// Import required modules
+const express = require('express');
+const app = express();
 
+// Define the port to run the server
+const port = 3000;
 
-this is new commiet for testing
+// Home route (GET request to '/')
+app.get('/', (req, res) => {
+    res.send('Welcome to the Home Page');
+});
+
+// About route (GET request to '/about')
+app.get('/about', (req, res) => {
+    res.send('This is the About Page');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
